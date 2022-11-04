31a999cb10eb15898d71338bf65910ba5dc99ae3
giannarao <98927149+giannarao@users.noreply.github.com>
2022-11-04 16:23:07 -0400

    average from input 2

--- averagefrominput.py
+++ averagefrominput.py
@@ -1,43 +1,32 @@
 #Gianna Rao
 #CS175L
-#AverageFromInput
+#AverageFromInput2
 
 def main():
-    infile = open("numbers.txt", "r")
-    
-    line1 = infile.readline()
-    line2 = infile.readline()
-    line3 = infile.readline()
-    
-    
-    line_1 = float(line1)
-    line_2 = float(line2)
-    line_3 = float(line3)
-    
-    total2 = (line_1 + line_2)
-    total3 = (line_1 + line_2 + line_3)
-    
-    average = (line_1+line_2+line_3)/3
-    
-    for x in range(1,2):
-        print("I read in", x, "number(s) Current number is: ", end="")
-        print(f'{line_1:.2f}', end=" ")
-        print("Total is: ", end="")
-        print(f'{line_1:.2f}')
-        
-        print("I read in", x+1, "number(s) Current number is: ", end="")
-        print(f'{line_2:.2f}', end=" ")
-        print("Total is: ", end="")
-        print(f'{total2:.2f}')
-        
-        print("I read in", x+2, "number(s) Current number is: ", end="")
-        print(f'{line_3:.2f}', end=" ")
-        print("Total is: ", end="")
-        print(f'{total3:.2f}')
-        
+    total = 0
+    x = 0
+    try:
+        infile = open("numbers.txt", "r")
+        for line in infile:
+            try:
+                amount = float(line)
+            except ValueError:
+                line = line.strip('\n')
+                print("Non-numeric data found in the file.")
+            else:
+                total = total + amount
+                x = x+1
+                print("I read in", x, "number(s) Current number is: ", end="")
+                print(f'{amount:.2f}', end=" ")
+                print("Total is: ", end="")
+                print(f'{total:.2f}')   
+        average = total/x    
         print("Average:", average)
-
-    infile.close
+            
+        infile.close
+        
+    except IOError:
+        print("An error occurred trying to read the file.")
         
 if __name__ == "__main__":
     main()
